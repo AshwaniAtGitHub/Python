38dd60054780f09d99bc73e675d803d516bef005
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2022-12-04 22:43:52 +0530

    variables & dataTypes

---
+++ 02_variables_and_dataType.py
@@ -0,0 +1,32 @@
+a=198
+print(a)
+
+b="Ashwani Loves White"
+print(b)
+
+a1=2
+print(a+a1)
+
+d=True
+print(d)
+
+e=10.45
+print(e)
+
+
+#complex number can also be assigned in python as following.
+
+comp = complex(7, 5)
+print(comp)
+
+list1 = [4, 5.8, 7, ["ashwni", "agraj", "abhishek"], ["banana", "apple", "pea"], 8, 9, 3]
+print(list1)
+
+
+print("The type of a is",type(a))
+print("The type of b is",type(b))
+print("The type of a1 is",type(a1))
+print("The type of d is",type(d))
+print("The type of e is",type(e))
+print("The type of comp is",type(comp))
+print("The type of list1 is",type(list1))
