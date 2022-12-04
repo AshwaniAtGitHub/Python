26514a35fa0da601563cda27687fe3ae0164298e
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2022-12-04 23:13:39 +0530

    Calculator

---
+++ 03_calculator.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first number:\n"))
+b = int(input("Enter second number:\n"))
+
+print("First number is ", a)
+print("Second number is ", b)
+
+print("The addition is ",a+b)
+print("The subtraction is ",a-b)
+print("The multiplication is ",a*b)
+print("The quotient is ",a/b)
+print("The integer quotient is ",a+b)
+print("The remander is ",a%b)
