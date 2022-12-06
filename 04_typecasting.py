4312badcb4d641123cb6cab378ca1d1415a171a6
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2022-12-06 23:36:48 +0530

    explicit conversion

--- 04_typecasting.py
+++ 04_typecasting.py
@@ -11,4 +11,8 @@
 
 '''
 
-
+string = "15"
+number = 7
+string_number = int(string) #throws an error if the string is not a valid integer
+sum= number + string_number
+print("The Sum of both the numbers is: ", sum)
