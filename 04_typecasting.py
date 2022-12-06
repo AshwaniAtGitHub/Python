2353118aae6d25648bd47d08f0cbce18b39796e0
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2022-12-06 23:50:32 +0530

    implicit typeCasting example

--- 04_typecasting.py
+++ 04_typecasting.py
@@ -27,3 +27,17 @@ print("The Sum of both the numbers is: ", sum)
     Python converts a smaller data type to a higher data type to prevent data loss.
 
 '''
+
+# Python automatically converts
+# a to int
+a = 7
+print(type(a))
+ 
+# Python automatically converts b to float
+b = 3.0
+print(type(b))
+ 
+# Python automatically converts c to float as it is a float addition
+c = a + b
+print(c)
+print(type(c))
