ac287310cb001f97d6a994302ba1bd98165a4a6a
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2022-12-06 23:35:44 +0530

    typeCasting

---
+++ 04_typecasting.py
@@ -0,0 +1,14 @@
+'''
+    Types of Typecasting
+    1. Explicit conversion
+    2. Implicit conversion
+
+    1. Explicit conversion
+
+    The conversion of one data type into another data type, done via developer's or progammer's intervention or manually as per the requirement, is known as explicit type conversion.
+
+    It can be achieved with the help of Python's build-in type conversion functions such as int(), float(), hex(), oct(), str(), etc.
+
+'''
+
+
