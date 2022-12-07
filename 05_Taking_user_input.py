d37871122f150fc341601a1599e0b9afc5c7ab2c
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2022-12-07 20:06:55 +0530

    Input function

---
+++ 05_Taking_user_input.py
@@ -0,0 +1,8 @@
+'''
+    syntax -> 
+    variable = input()
+
+    input function returns the value as a string. Hence, we have to typecast them whenever required to another data type.
+
+'''
+#Example->
