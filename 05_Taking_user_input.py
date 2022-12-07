c4c05469c74d3ea2cc84a60d04d70288d3149421
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2022-12-07 21:46:34 +0530

    Taking User Input

--- 05_Taking_user_input.py
+++ 05_Taking_user_input.py
@@ -6,3 +6,12 @@
 
 '''
 #Example->
+a = input("Enter your name: ")
+print("My name is", a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+
+print (x+y)
+
+print(int(x) + int(y))
