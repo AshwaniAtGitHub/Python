84e13c62474e8300eb5455e33beaef89a056bd8c
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2022-12-12 23:50:05 +0530

    String_Methods

--- 08_string_method.py
+++ 08_string_method.py
@@ -20,4 +20,12 @@ print(len(note.center(50)))
 
 print(note.count("World"))
 print(heading.endswith("python"))
+print(heading.endswith("to",4,10))
+print(heading.find("to")) #returns first occurance of string.
+print(heading.find("toy")) #returns -1 if string not found.
+
+print(heading.isalnum()) #returns True if the string is alfanumeric else False.
+
+heading2 = "welcomeTo21stStreet"
+print(heading2.isalnum()) #returns True if the string is alfanumeric else False.
 
