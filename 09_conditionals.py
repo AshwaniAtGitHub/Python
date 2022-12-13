c8b633dbad1d3bfba7ef9c124938be4ea14d5ac5
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2022-12-13 19:48:44 +0530

    Conditionals

---
+++ 09_conditionals.py
@@ -0,0 +1,24 @@
+# Sometimes the programmer needs to check the evaluation of certain expression(s), whether the expression(s) evaluate to True or False. If the expression evaluates to False, then the program execution follows a different path than it would have if the expression had evaluated to True.
+
+# Based on this, the conditional statements are further classified into following types:
+
+# if
+# if-else
+# if-else-elif
+# nested if-else-elif.
+
+
+a = int(input("Enter your age: "))
+print("Your age is:", a)
+
+# Conditional operators 
+# >, <, >=, <=, ==, !=
+
+if(a>18):
+  print("You can drive")
+  print("Yes")
+else:
+  print("You cannot drive")
+  print("No")
+
+  
