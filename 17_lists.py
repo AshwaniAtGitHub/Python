b5b44d66148f9c74def3ced6d79a4bfbffd771d6
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2022-12-23 23:42:29 +0530

    Lists

--- 17_lists.py
+++ 17_lists.py
@@ -2,3 +2,40 @@
 # They store multiple items in a single variable.
 # List items are separated by commas and enclosed within square brackets [].
 # Lists are changeable meaning we can alter them after creation.
+
+
+
+marks = [3, 5, 6, "Ashwani", True, 6, 7 , 2, 32, 345, 23]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(marks[2])
+print(marks[3])
+print(marks[4])
+print(marks[5])
+
+print(marks[-3]) # Negative index
+print(marks[len(marks)-3]) # Positive index
+print(marks[5-3]) # Positive index
+print(marks[2]) # Positive index
+
+if "6" in marks:
+  print("Yes")
+else:
+  print("No")
+
+# Same thing applies for strings as well!
+
+if "Sun" in "Sunny":
+  print("Yes")
+
+print(marks[0:7])
+print(marks[1:9])
+print(marks[1:9:3])
+
+lst = [i*i for i in range(10)]
+print(lst)
+lst = [i*i for i in range(10) if i%2==0]
+print(lst)
+
