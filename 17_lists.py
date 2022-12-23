3885aa0fd04a105a062a62903dfe41dc88a591cf
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2022-12-23 23:33:40 +0530

    lists definition

---
+++ 17_lists.py
@@ -0,0 +1,4 @@
+# Lists are ordered collection of data items.
+# They store multiple items in a single variable.
+# List items are separated by commas and enclosed within square brackets [].
+# Lists are changeable meaning we can alter them after creation.
