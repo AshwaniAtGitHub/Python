8f55e00a6a21d07f51dc72a861220238a4e2c150
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2023-01-04 00:33:18 +0530

    check for an item in tuple

--- 19_tuples.py
+++ 19_tuples.py
@@ -55,3 +55,24 @@ country = ("Spain", "Italy", "India", "England", "Germany")
 print(country[-1]) # Similar to print(country[len(country) - 1])
 print(country[-3])
 print(country[-4])
+
+# III. Check for item:
+
+# We can check if a given item is present in the tuple. This is done using the in keyword.
+
+# Example 1:
+
+country = ("Spain", "Italy", "India", "England", "Germany")
+if "Germany" in country:
+    print("Germany is present.")
+else:
+    print("Germany is absent.")
+
+# Example 2:
+
+country = ("Spain", "Italy", "India", "England", "Germany")
+if "Russia" in country:
+    print("Russia is present.")
+else:
+    print("Russia is absent.")
+
