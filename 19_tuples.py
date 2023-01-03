1bc966b2b974a792eeea598469b1a71618212fcf
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2023-01-04 00:29:41 +0530

    negative indexing in tuples

--- 19_tuples.py
+++ 19_tuples.py
@@ -45,3 +45,13 @@ print(country[1])
 print(country[2])
 
 
+# II. Negative Indexing:
+# Similar to positive indexing, negative indexing is also used to access items, but from the end of the tuple. The last item has index [-1], second last item has index [-2], third last item has index [-3] and so on.
+
+# Example:
+
+country = ("Spain", "Italy", "India", "England", "Germany")
+#            [0]      [1]      [2]       [3]        [4]
+print(country[-1]) # Similar to print(country[len(country) - 1])
+print(country[-3])
+print(country[-4])
