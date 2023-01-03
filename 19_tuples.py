fb8f9c682e0f775cc7f4ed05013a01c4727eaade
AshwaniAtGitHub <ashwanikumarmaurya8127@gmail.com>
2023-01-04 00:43:56 +0530

    range of index

--- 19_tuples.py
+++ 19_tuples.py
@@ -76,3 +76,46 @@ if "Russia" in country:
 else:
     print("Russia is absent.")
 
+# IV. Range of Index:
+# You can print a range of tuple items by specifying where do you want to start, where do you want to end and if you want to skip elements in between the range.
+
+# Syntax:
+# Tuple[start : end : jumpIndex]
+# Note: jump Index is optional. We will see this in given examples.
+
+# Example: Printing elements within a particular range:
+animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
+print(animals[3:7])     #using positive indexes
+print(animals[-7:-2])   #using negative indexes
+
+# Here, we provide index of the element from where we want to start and the index of the element till which we want to print the values. Note: The element of the end index provided will not be included.
+
+# Example: Printing all element from a given index till the end
+
+animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
+
+print(animals[4:])      #using positive indexes
+print(animals[-4:])     #using negative indexes
+
+
+# When no end index is provided, the interpreter prints all the values till the end.
+
+# Example: printing all elements from start to a given index
+
+animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
+print(animals[:6])      #using positive indexes
+print(animals[:-3])     #using negative indexes
+
+# Example: Print alternate values
+
+animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
+
+print(animals[::2])     #using positive indexes
+print(animals[-8:-1:2]) #using negative indexes
+
+# Here, we have not provided start and end index, which means all the values will be considered. But as we have provided a jump index of 2 only alternate values will be printed.
+
+# Example: printing every 3rd consecutive withing given range
+
+animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
+print(animals[1:8:3])
